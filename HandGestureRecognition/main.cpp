bf1195892ea1b7e3340bf45467abd0308a27ada5
pranay3 <panay.kumar26@gmail.com>
2013-10-20 12:43:46 +0530

    second commit. Main function added

---
+++ HandGestureRecognition/main.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/core/core.hpp>
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <cmath>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cout<<"Team- Vision"<<endl;
+	return(0);
+}
